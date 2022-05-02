746893a3aa020d25d52778cede0b4477fa6ff165
Lovász Bence <lovasz.b99@gmail.com>
2022-05-03 00:31:07 +0200

    second window viewmodel

---
+++ FV8H3R_HFT_2021221.WpfClient/MatchWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using FV8H3R_HFT_2021221.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FV8H3R_HFT_2021221.WpfClient
+{
+    public class MatchWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Match> Matches { get; set; }
+
+        private Match selectedMatch;
+
+        public Match SelectedMatch
+        {
+            get { return selectedMatch; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedMatch = value;
+                }    
+            }
+        }
+
+        public static bool IsInDesignMode
+        {
+            get { return (bool)DependencyPropertyDescriptor.FromProperty(DesignerProperties.IsInDesignModeProperty, typeof(System.Windows.FrameworkElement)).Metadata.DefaultValue; }
+        }
+
+        public ICommand AddMatchCommand { get; set; }
+        public ICommand RemoveMatchCommand { get; set; }
+        public ICommand UpdateMatchCommand { get; set; }
+
+        public MatchWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Matches = new RestCollection<Match>("http://localhost:48623", "match");
+
+                AddMatchCommand = new RelayCommand(
+                    () => Matches.Add(new Match()
+                        {
+                            Id = SelectedMatch.Id,
+                            User_1 = SelectedMatch.User_1,
+                            User_2 = SelectedMatch.User_2
+                        }
+                    ),
+                    () => SelectedMatch != null);
+
+                RemoveMatchCommand = new RelayCommand(
+                    () => Matches.Delete(SelectedMatch.Id),
+                    () => SelectedMatch != null);
+
+                UpdateMatchCommand = new RelayCommand(
+                    () => Matches.Update(SelectedMatch),
+                    () => SelectedMatch != null);
+            }
+        }
+    }
+}
