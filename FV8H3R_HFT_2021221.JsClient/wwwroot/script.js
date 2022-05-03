84e68e45890c503ee56b1d46605e8e6dd6a717ea
Lovász Bence <lovasz.b99@gmail.com>
2022-05-03 02:34:31 +0200

    added js project

---
+++ FV8H3R_HFT_2021221.JsClient/wwwroot/script.js
@@ -0,0 +1,79 @@
+﻿let connection;
+let url = "http://localhost:37822/user";
+
+let log = console.log;
+
+let Users = Array();
+
+$("document").ready(function () {
+    function SetupSignalR() {
+        connection = new signalR.HubConnectionBuilder()
+            .withUrl(url)
+            .configureLogging()
+            .build();
+
+        connection.on("User created", (user, msg) => GetData());
+        connection.on("User deleted", (user, msg) => GetData());
+
+        connection.onclose(async () => await Start());
+
+        Start();
+    }
+
+    async function Start() {
+        try {
+            await connection.Start();
+        }
+        catch (error) {
+            log(error);
+            setTimeout(start, 2500);
+        }
+    }
+
+    async function GetData() {
+        log("GetData");
+        await fetch(url)
+            .then(x => x.json())
+            .then(y => { Users = y; Display(); });
+    }
+
+    function Display() {
+        $("#results").html("");
+    }
+
+    function Post() {
+        var name = $("#DogName").val();
+
+        await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ UserName: name }),
+        })
+            .then(response => response)
+            .then(data => { log("User added"); GetData(); })
+            .catch(error => log("Error: ", error));
+    }
+
+    function Remove(Id) {
+        fetch(url + Id, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+            body: null,
+        })
+            .then(response => response)
+            .then(data => { log("User deleted"); GetData(); })
+            .catch(error => log("Error: ", error));
+    }
+
+    function Update(Id) {
+        var name = $(Id + ".Name").val()
+        fetch(url + Id, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({UserId: Id, UserName: name}),
+        })
+            .then(response => response)
+            .then(data => { log("User updated"); GetData(); })
+            .catch(error => log("Error: ", error));
+    }
+});
